a9f1fa17e3954c3b957318337c32cefb8293336c
hLeePage <hleepage@yahoo.com>
2015-10-26 17:14:15 -0700

    Add alerts and hurricanes

--- main.rb
+++ main.rb
@@ -2,40 +2,45 @@ require 'httparty'
 require 'json'
 require 'open-uri'
 require 'pry'
-
+KEY = 'dde604e560c795e9'
 #puts "Please enter your zipcode: "
 #zip = gets.chomp
 zip = 37205
 
+
 # Alerts
-response = HTTParty.get("http://api.wunderground.com/api/dde604e560c795e9/alerts/q/#{zip}.json")
+response = HTTParty.get("http://api.wunderground.com/api/#{KEY}/alerts/q/#{zip}.json")
 alerts = response['alerts']
 # lzy bstrd
 puts "There are no alerts for your area."
 
 
 # Current conditions
-response = HTTParty.get("http://api.wunderground.com/api/dde604e560c795e9/geolookup/conditions/q/#{zip}.json")
+response = HTTParty.get("http://api.wunderground.com/api/#{KEY}/geolookup/conditions/q/#{zip}.json")
 location = response['location']['city']
 temp_f = response['current_observation']['temp_f']
-puts "Current temperature in #{location} is: #{temp_f}\n"
+puts "Current temperature in #{location} is: #{temp_f} degrees Farenheit.\n"
 
 
 # Sunrise/sunset
-response = HTTParty.get("http://api.wunderground.com/api/dde604e560c795e9/astronomy/q/#{zip}.json")
+response = HTTParty.get("http://api.wunderground.com/api/#{KEY}/astronomy/q/#{zip}.json")
 rise_hour = response['sun_phase']['sunrise']['hour']
 rise_min = response['sun_phase']['sunrise']['minute']
 set_hour = response['sun_phase']['sunset']['hour']
 set_min = response['sun_phase']['sunset']['minute']
-puts "Sunrise at #{rise_hour}:#{rise_min}"
-puts "Sunset at #{set_hour}:#{set_min}"
+puts "Sunrise at #{rise_hour}:#{rise_min}."
+puts "Sunset at #{set_hour}:#{set_min}."
 
 
 # 10 Day Forecast
-response = HTTParty.get("http://api.wunderground.com/api/dde604e560c795e9/forecast10day/q/#{zip}.json")
+response = HTTParty.get("http://api.wunderground.com/api/#{KEY}/forecast10day/q/#{zip}.json")
 #binding.pry
 10.times do |i|
 day = response['forecast']['txt_forecast']['forecastday'][i]['title']
 conditions = response['forecast']['txt_forecast']['forecastday'][i]['fcttext']
-puts "Conditions for #{day} :#{conditions}"
+puts "Conditions for #{day}: #{conditions}"
 end
+
+response = HTTParty.get("http://api.wunderground.com/api/#{KEY}/currenthurricane/view.json")
+hurricanes = response['currenthurricane'][0]['stormInfo']['stormName']
+puts "Current hurricanes: #{hurricanes}."
