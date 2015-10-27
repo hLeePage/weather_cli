e8bcffaa79364eaaa4469bb2a171da1ca13f5a34
hLeePage <hleepage@yahoo.com>
2015-10-27 16:19:46 -0700

    .ten_day appears to work

--- ten_day.rb
+++ ten_day.rb
@@ -1,16 +1,17 @@
 class TenDay
 
-  attr_accessor :day, :conditions
+  attr_accessor :arr
 
   def self.parse(data)
     obj = TenDay.new
 
+    arr = []
     20.times do |i|
       day = data['forecast']['txt_forecast']['forecastday'][i]['title']
       conditions = data['forecast']['txt_forecast']['forecastday'][i]['fcttext']
-      obj[day] = [conditions]
+      arr << [day, conditions]
     end
-    obj
+    obj.arr = arr
   end
 
 end
